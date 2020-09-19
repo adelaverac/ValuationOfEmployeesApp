1f9141d9fceb226085a8f9df965f218ea4444ef5
Andy de la Vera Castro <adelaverac@hotmail.com>
2020-09-18 22:57:42 -0500

    Implementación de splash al iniciar la aplicación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from "@ionic/angular";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { timer } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+  constructor(private platform: Platform, private statusBar: StatusBar) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
-      this.statusBar.backgroundColorByHexString('#fff');
+      this.statusBar.backgroundColorByHexString("#fff");
+      const splash: HTMLElement = document.getElementById("splash");
+      timer(5000).subscribe(() => {
+        splash.style.display = "none";
+      });
     });
   }
 }
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,21 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { IonicModule } from '@ionic/angular';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HeaderComponent } from "./header/header.component";
+import { IonicModule } from "@ionic/angular";
+import { SplashComponent } from "./splash/splash.component";
 
 @NgModule({
-  declarations: [
-    HeaderComponent
-  ],
-  exports:[
-    HeaderComponent
-
-  ],
-  imports: [
-    CommonModule,
-    IonicModule
-  ]
+  declarations: [HeaderComponent, SplashComponent],
+  exports: [HeaderComponent, SplashComponent],
+  imports: [CommonModule, IonicModule],
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
